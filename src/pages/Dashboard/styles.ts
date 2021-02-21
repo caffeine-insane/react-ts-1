9b2874aadf37255c50262242df94c44fcdd096ab
Gabriel Doyle Balk <gdoyle.dev@gmail.com>
2021-02-21 20:27:14 -0300

    add styled components

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+
+export const Title = styled.h1`
+    color: #3A3A3A;
+    font-size: 48px;
+`;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,12 +5,11 @@ import { Switch, Route } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Repository from '../pages/Repository';
 
-
 const Routes: React.FC = () => (
-    <Switch>
-        <Route path="/" exact component={Dashboard} />
-        <Route path="/repository" component={Repository} />
-    </Switch>
+  <Switch>
+    <Route path="/" exact component={Dashboard} />
+    <Route path="/repository" component={Repository} />
+  </Switch>
 );
 
 export default Routes;
